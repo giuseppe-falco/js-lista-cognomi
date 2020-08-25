f48926af2a8301e53717c510af080a1e42ba916d
joseph <giuseppe.falco000@gmail.com>
2020-08-25 16:38:47 +0200

    end

--- js/script.js
+++ js/script.js
@@ -5,22 +5,37 @@
 // scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
 
 //richiesta cognome
-var cognome = prompt("Inserisci il tuo cognome !");
+var cognome = prompt("Inserisci il tuo cognome!");
 
 //lista cognomi
 var listaCognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
 
 //inserimento in array
-listaCognomi.push(cognome.charAt(0).toUpperCase() + cognome.slice(1));
+    //prima lettera maiuscola 
+    cognome = (cognome.charAt(0).toUpperCase() + cognome.slice(1));
+listaCognomi.push(cognome);
 
-console.log(listaCognomi)
-
+//ordinamento lista cognomi
 var ordine = listaCognomi.sort();
+
+
 //stampo lista
 for(var i = 0; i < listaCognomi.length; i++) {
     document.getElementById('lista-cognomi').innerHTML += "<li>" + ordine[i] + "</li>";
 }
 
-//non valida
+
+//calcolo posizione corrente
+i = 0;
+var position, condition = false;
+
+while (condition == false) {
+  if (cognome == listaCognomi[i]) {
+    condition = true;
+    position = ++i;
+  }
+  i++;
+}
+
 //stampa posizione corrente
-alert("Il tuo cognome si trova nella posizione n." + i);
+alert("Il tuo cognome si trova nella posizione n." + position);
